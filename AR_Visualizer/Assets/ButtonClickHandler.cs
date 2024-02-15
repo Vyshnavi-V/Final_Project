adca91f0eeac37a56481e3037a9210f4d87d9b6d
Lekshmi Priya V <85623989+lekshmi-02@users.noreply.github.com>
2024-02-15 20:29:26 +0530

    updated buttons

---
+++ AR_Visualizer/Assets/ButtonClickHandler.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+public class ButtonClickHandler : MonoBehaviour
+{
+    public void OnButtonClick()
+    {
+        Debug.Log("Button Clicked!");
+        // Add your logic here to handle the button click
+    }
+}
